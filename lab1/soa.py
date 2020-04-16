e09bd374780eebb42139b9575b9ecf9202019798
siusta <56389581+siusta@users.noreply.github.com>
2020-04-16 21:46:21 +0200

    fix

--- lab1/soa.py
+++ lab1/soa.py
@@ -3,4 +3,5 @@ from osa import Client
 cl = Client("http://localhost:8088/mockApiPortSoap11?WSDL")
 reg_call = cl.service.registerCall(" ")
 res = cl.service.results(" ")
-print(reg_call,res)
+print("Name: " + reg_call.name)
+print("Note: " + reg_call.note)
